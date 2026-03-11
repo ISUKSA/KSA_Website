afb73812cbbb37de836fc79e60578f197676d3a4
isuksa88 <isuksa88@gmail.com>
2026-03-10 21:24:56 -0500

    Create CampusGuidePage.tsx

---
+++ src/app/pages/CampusGuidePage.tsx
@@ -0,0 +1,9 @@
+import { CampusGuide } from '@/app/components/CampusGuide';
+
+export function CampusGuidePage() {
+  return (
+    <div className="pt-16">
+      <CampusGuide />
+    </div>
+  );
+}
